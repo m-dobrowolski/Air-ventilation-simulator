83f26119927eb56d0f5d3c7f698b22ea5c08b036
Maciej Dobrowolski <maciejd644@gmail.com>
2025-01-04 22:19:00 +0100

    Working air flow (from map)

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import json
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
+# define types of tiles
+OUTSIDE = 0
+OBSTACLE = 1
+INTERIOR = 2
+FLOW_RIGHT = 3
+FLOW_UP = 4
+FLOW_LEFT = 5
+FLOW_DOWN = 6
+
+SKIP_ITERATIONS = 0 #400
+
 def load_map(path):
     with open(path, 'r') as fh:
         data = fh.read()
@@ -11,7 +22,8 @@ def load_map(path):
     return np.array(map_data)
 
 def main():
-    map_data = load_map('object_editor/maps/mymap')
+    # map_data = load_map('object_editor/maps/mymap')
+    map_data = load_map('object_editor/maps/mapmapmap')
 
     # Simulation parameters
     Nx          = 200 #map_data["Nx"]    # resolution x-dir
@@ -25,34 +37,42 @@ def main():
     cys = np.array([0, 1, 1, 0,-1,-1,-1, 0, 1])
     weights = np.array([4/9,1/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36]) # sums to 1
 
+    def mask(shape, map_data, required_type):
+        data = np.full(shape, False)
+        for y, row in enumerate(map_data):
+            start_y = y * 10
+            end_y = start_y + 10
+            for x, type in enumerate(row):
+                start_x = x * 10
+                end_x = start_x + 10
+                if type == required_type:
+                    data[start_y:end_y, start_x:end_x] = True
+            if required_type == FLOW_DOWN:
+                print(f'{start_y}:{end_y}')
+                print(row)
+        return data
+
+    shape = (Ny, Nx)
+
+    outside = mask(shape, map_data, OUTSIDE)
+    obstacles = mask(shape, map_data, OBSTACLE)
+    interior = mask(shape, map_data, INTERIOR)
+    flow_right = mask(shape, map_data, FLOW_RIGHT)
+    flow_up = mask(shape, map_data, FLOW_UP)
+    flow_left = mask(shape, map_data, FLOW_LEFT)
+    flow_down = mask(shape, map_data, FLOW_DOWN)
+    print(flow_down)
+    is_flow_right = flow_right.any()
+    is_flow_up = flow_right.any()
+    is_flow_left = flow_right.any()
+    is_flow_down = flow_right.any()
+
     # Initial Conditions - flow to the right with some perturbations
-    F = np.ones((Ny,Nx,NL)) + 0.01*np.random.randn(Ny,Nx,NL)
-    F[:,:, 3] = 2.3
-
-    # Cylinder boundary
-    # def distance(x1, y1, x2, y2):
-    #     return np.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-    # cylinder = np.full((Ny, Nx), False)
-    # for y in (0, Ny):
-    #     for x in range (0, Nx):
-    #         if(distance(Nx//4, Ny//2, x, y) < 13):
-    #             cylinder[y][x] = True
-
-    obstacles = np.full((Ny, Nx), False)
-
-    for y, row in enumerate(map_data):
-        start_y = y * 10
-        end_y = start_y + 10
-        for x, type in enumerate(row):
-            start_x = x * 10
-            end_x = start_x + 10
-            if type == 1:
-                obstacles[start_y:end_y, start_x:end_x] = True
-
+    F = np.ones((Ny,Nx,NL)) + 0.03*np.random.randn(Ny,Nx,NL)
+    # F[outside, 3] = 1.5
+    # F[interior, :] = 1
 
     # plt.ion()
-
     # Simulation Main Loop
     for it in tqdm(range(Nt)):
         # Absorbing boundaries
@@ -61,6 +81,16 @@ def main():
         F[0, :, [1, 2, 8]] = F[1, :, [1, 2, 8]]
         F[-1, :, [4, 5, 6]] = F[-2, :, [4, 5, 6]]
 
+        # Air flow
+        if is_flow_right:
+            F[flow_right, 3] = 1.5
+        if is_flow_up:
+            F[flow_up, 1] = 1.5
+        if is_flow_left:
+            F[flow_left, 7] = 1.5
+        if is_flow_down:
+            F[flow_down, 5] = 1.5
+
 
         # Drift
         for i, cx, cy in zip(range(NL), cxs, cys):
@@ -68,8 +98,9 @@ def main():
             F[:,:,i] = np.roll(F[:,:,i], cy, axis=0)
 
         # Set reflective boundaries
-        bndryF = F[obstacles,:]
-        bndryF = bndryF[:,[0,5,6,7,8,1,2,3,4]]
+        if it > 1:
+            bndryF = F[obstacles,:]
+            bndryF = bndryF[:,[0,5,6,7,8,1,2,3,4]]
 
         # Calculate fluid variables
         rho = np.sum(F,2)
@@ -77,7 +108,8 @@ def main():
         uy  = np.sum(F*cys,2) / rho
 
         # F[obstacles, :] = bndryF
-        F[obstacles, :] = bndryF
+        if it > 1:
+            F[obstacles, :] = bndryF
         ux[obstacles] = 0
         uy[obstacles] = 0
 
@@ -89,13 +121,16 @@ def main():
 
         F += -(1.0/tau) * (F - Feq)
 
-        # Apply boundary
-        F[obstacles,:] = bndryF
+        # dfydx = ux[2:, 1:-1] - ux[0:-2, 1:-1]
+        # dfxdy = uy[1:-1, 2:] - uy[1:-1, 0:-2]
+        # curl = dfydx - dfxdy
 
-        # if it % 100 == 0:
-        # plt.imshow(np.sqrt(ux**2+uy**2))
-        # plt.pause(0.001)
-        # plt.cla()
+        # if it >= SKIP_ITERATIONS:
+        #     plt.imshow(np.sqrt(ux**2+uy**2), origin='lower')
+        #     plt.legend()
+
+        #     plt.pause(0.001)
+        #     plt.cla()
 
         if it % 100 == 0:
             plt.imshow(np.sqrt(ux**2+uy**2))
