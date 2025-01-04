f74116cd3381deacec2f0fb4cfa703967404e9a1
Maciej Dobrowolski <maciejd644@gmail.com>
2025-01-04 23:46:30 +0100

    renamed some files

--- level_editor/settings.py
+++ level_editor/settings.py
@@ -4,7 +4,7 @@ class Settings:
         self.tiles_num_border = 20
         self.screen_width, self.screen_height = self.tiles_num_border * 32, self.tiles_num_border * 32
         # Tiles settings
-        self.tileset_file = "images/tileset.png"
+        self.tileset_file = "/level_editor/images/tileset.png"
         self.tileset_margin = 1
         self.tileset_spacing = 1
         self.tile_size = (32, 32)  # width, height
